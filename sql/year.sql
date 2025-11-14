89fc9b8e460378833061659f73cf6070f54f3a4a
ngntrgduc <trungducnguyen1511@gmail.com>
2025-11-14 11:08:10 +0700

    Using movie instead of movie_detail for year.sql

--- sql/year.sql
+++ sql/year.sql
@@ -4,7 +4,7 @@ SELECT
     COUNT(*) AS count,
     SUM(CASE WHEN status = 'completed' THEN 1 ELSE 0 END) AS completed,
     ROUND(AVG(rating), 2) AS avg_rating
-FROM movie_detail md
+FROM movie m
 -- WHERE rating IS NOT NULL
 GROUP BY year
 -- HAVING avg_rating IS NOT NULL;
