cf904e37737d1606b60bfaa09c4d669abe562b3c
ngntrgduc <trungducnguyen1511@gmail.com>
2025-11-14 11:02:36 +0700

    Add watched_percent and improve readability by CTE

--- sql/genres.sql
+++ sql/genres.sql
@@ -1,19 +1,22 @@
--- List all genres in the database
--- SELECT * FROM genre;
-
 -- Genres statistics
+WITH movie_flags AS (
+    SELECT
+        m.id,
+        m.rating,
+        CASE 
+            WHEN m.status IN ('completed', 'dropped') THEN 1 ELSE 0 
+        END AS watched_flag
+    FROM movie m
+)
 SELECT 
     g.id, 
     g.name, 
     COUNT(*) AS count, 
-    SUM(
-        CASE 
-            WHEN m.status IN ('completed', 'dropped') THEN 1 ELSE 0 
-        END
-    ) AS watched,
-    ROUND(AVG(rating), 2) AS avg_rating
+    SUM(mf.watched_flag) AS watched,
+    ROUND(100.0 * SUM(mf.watched_flag) / COUNT(*), 2) || '%' AS watched_percent,
+    ROUND(AVG(mf.rating), 2) AS avg_rating
 FROM genre AS g
 LEFT JOIN movie_genre mg ON mg.genre_id = g.id
-LEFT JOIN movie m ON m.id = mg.movie_id
+LEFT JOIN movie_flags mf ON mf.id = mg.movie_id
 GROUP BY g.name
 ORDER BY count DESC;
