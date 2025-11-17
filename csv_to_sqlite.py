dbdf45e719a273f35a27ac28e166e88f7009e404
ngntrgduc <trungducnguyen1511@gmail.com>
2025-11-17 21:28:07 +0700

    Handle deleting database if exists

--- csv_to_sqlite.py
+++ csv_to_sqlite.py
@@ -2,6 +2,7 @@ import sqlite3
 import pandas as pd
 from utils.movie import add_movie
 from time import perf_counter
+from pathlib import Path
 
 tic = perf_counter()
 def csv_to_sqlite(df: pd.DataFrame, con: sqlite3.Connection) -> None:
@@ -16,7 +17,19 @@ def csv_to_sqlite(df: pd.DataFrame, con: sqlite3.Connection) -> None:
         movie_dict['rating'] = float(movie['rating']) if not pd.isna(movie['rating']) else None
         add_movie(movie_dict, cur)
 
-con = sqlite3.connect("data/movies.db")
+DB_PATH = Path('data/movies.db')
+CSV_PATH = Path('data/data.csv')
+
+if DB_PATH.exists():
+    print(f'Database file {DB_PATH} still exists, deleting...')
+    try:
+        DB_PATH.unlink()
+        print(f'Deleted database file. Re-creating it with data from {CSV_PATH}...')
+    except PermissionError:
+        print('Cannot delete database: it is open in another program. Close it first.')
+        raise SystemExit
+
+con = sqlite3.connect(DB_PATH)
 cur = con.cursor()
 with open('sql/schema.sql', 'r') as f:
     schema_sql = f.read()
@@ -24,9 +37,9 @@ with open('sql/schema.sql', 'r') as f:
 cur.executescript(schema_sql)
 con.commit()
 
-df = pd.read_csv('data/data.csv')
+df = pd.read_csv(CSV_PATH)
 csv_to_sqlite(df, con)
 con.commit()
 
 con.close()
-print(f'Moved data from CSV to SQLite database, took {(perf_counter() - tic):.4f}s')
+print(f'Moved data from CSV to SQLite database, took {(perf_counter() - tic):.4f}s.')
