894a7cf413514f5e8ed57208488e8c0199e269f5
ngntrgduc <trungducnguyen1511@gmail.com>
2025-11-17 13:04:47 +0700

    Change latest.sql to get latest 15 rows

--- sql/latest.sql
+++ sql/latest.sql
@@ -1,7 +1,7 @@
--- get 10 latest added rows in the database
+-- get 15 latest rows in the database
 SELECT * FROM (
     SELECT * FROM movie_detail 
     ORDER BY id DESC 
-    LIMIT 10
+    LIMIT 15
 ) 
 ORDER BY id ASC;
