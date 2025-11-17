470fc7dbcf3531efc4737443a18b74c9d88a14ae
ngntrgduc <trungducnguyen1511@gmail.com>
2025-11-17 21:14:32 +0700

    Update file name of Add and Edit page after rename

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 pg = st.navigation(
     [
         st.Page("pages/1_data.py", title="Data"),  # number before name for ordering purpose
-        st.Page("pages/add_movie.py", title="Add"),
-        st.Page("pages/edit_movie.py", title="Edit"),
+        st.Page("pages/2_add_movie.py", title="Add"),
+        st.Page("pages/3_edit_movie.py", title="Edit"),
     ],
     position="top"
 )
