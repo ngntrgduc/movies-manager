fed015b9203be142d062e3952dde11be3d4f2013
ngntrgduc <trungducnguyen1511@gmail.com>
2025-11-19 20:45:59 +0700

    SQL: add count and percent columns

--- sql/watchedyear.sql
+++ sql/watchedyear.sql
@@ -1,5 +1,7 @@
 SELECT 
     SUBSTR(watched_date, 1, 4) AS year, 
+    COUNT(*) AS count,
+    ROUND(100.0 * COUNT(*) / (SELECT COUNT(*) FROM movie), 2) || '%' AS percent,
     SUM(CASE WHEN type = 'movie' THEN 1 ELSE 0 END) AS movie_count,
     SUM(CASE WHEN type = 'series' THEN 1 ELSE 0 END) AS series_count,
     ROUND(AVG(rating), 2) AS avg_rating
