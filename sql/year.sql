56d0c8d560826f229b0864dac11f6a574d6f3474
ngntrgduc <trungducnguyen1511@gmail.com>
2025-11-19 20:32:02 +0700

    SQL: uppercase SQL keyword

--- sql/year.sql
+++ sql/year.sql
@@ -3,7 +3,7 @@ WITH year_stats AS (
     SELECT 
         year,
         COUNT(*) AS count,
-        SUM(CASE WHEN status in ('completed', 'dropped') THEN 1 ELSE 0 END) AS watched,
+        SUM(CASE WHEN status IN ('completed', 'dropped') THEN 1 ELSE 0 END) AS watched,
         ROUND(AVG(rating), 2) AS avg_rating
     FROM movie
     GROUP BY year
@@ -13,6 +13,6 @@ SELECT
     year,
     count,
     watched,
-    ROUND(100.0 * watched / count, 2) || '%' as percent, 
+    ROUND(100.0 * watched / count, 2) || '%' AS percent, 
     avg_rating
 FROM year_stats
