9ee7363703bfb4a04b4a15e71b8224ddfd7bfd83
ngntrgduc <trungducnguyen1511@gmail.com>
2025-11-25 21:36:04 +0700

    Allow fetch_rows to run parameterized SQL query

--- utils/db.py
+++ utils/db.py
@@ -4,9 +4,15 @@ def fetch_scalar(cur: sqlite3.Cursor, query: str) -> int | float:
     """Run a SQL query and return its single scalar value."""
     return cur.execute(query).fetchone()[0]
 
-def fetch_rows(cur: sqlite3.Cursor, query: str) -> tuple[list[tuple], list[str]]:
-    """Run a SQL query and return its rows and column names."""
-    cur.execute(query)
+def fetch_rows(
+    cur: sqlite3.Cursor, query: str, parameter: tuple = None
+) -> tuple[list[tuple], list[str]]:
+    """Run a (parameterized) SQL query and return its rows and column names."""
+    if parameter:
+        cur.execute(query, parameter)
+    else:
+        cur.execute(query)
+
     rows = cur.fetchall()
     column_names = [d[0] for d in cur.description]
     return rows, column_names
