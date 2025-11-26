c4e0014e13968dec24e3d7f486173b3165b6b72a
ngntrgduc <trungducnguyen1511@gmail.com>
2025-11-26 13:42:46 +0700

    Remove sql/recent.sql, use recent command instead

--- sql/recent.sql
+++
@@ -1,8 +0,0 @@
--- List 10 recent watched
-SELECT * FROM (
-    SELECT * FROM movie_detail 
-    WHERE status in ('completed', 'dropped')
-    ORDER BY id DESC 
-    LIMIT 10
-) 
-ORDER BY watched_date;
