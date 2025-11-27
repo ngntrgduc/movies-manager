55006343d029bf9d0171b826c5d7c842b23433de
ngntrgduc <trungducnguyen1511@gmail.com>
2025-11-27 22:34:31 +0700

    Add file utilities: get_file_size, convert_bytes

---
+++ utils/file.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+def convert_bytes(size: int) -> str:
+    """Convert bytes to KB, MB, GB, TB."""
+    for unit in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if size < 1024:
+            return f'{size:.1f} {unit}'
+        size /= 1024
+
+def get_file_size(file: str | Path) -> int:
+    """Return file size as raw bytes (int)."""
+
+    if isinstance(file, str):
+        file = Path(file)
+
+    return file.stat().st_size
+
+# if __name__ == '__main__':
+#     data_folder = Path('data/')
+#     total = 0
+#     for data_file in data_folder.iterdir():
+#         # print(f'{data_file}: {get_file_size(data_file, human_readable=True)}')
+#         total += get_file_size(data_file)
+#     print(convert_bytes(total))
