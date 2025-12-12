670c03897e8975d25c8d95a28cec1b03a1634062
ngntrgduc <trungducnguyen1511@gmail.com>
2025-12-12 22:07:18 +0700

    Move valid_date testing to test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,5 @@
 import pytest
-from utils.cli import resolve_choice
+from utils.cli import resolve_choice, valid_date
 from utils.constants import MOVIE_STATUSES, MOVIE_TYPES, COUNTRIES
 
 def test_resolve_choice():
@@ -15,3 +15,18 @@ def test_invalid_choice():
 def test_same_initial():
     with pytest.xfail(reason="Resolve choice can't handle same initials yet"):
         assert resolve_choice('a', ['abc', 'abcd', 'aaa']) == 'abc'
+
+def test_valid_date():
+    assert valid_date('2025') == '2025'
+    assert valid_date('2025-12') == '2025-12'
+    assert valid_date('2025-12-06') == '2025-12-06'
+
+def test_valid_date_with_empty_input():
+    assert valid_date('') == ''
+    assert valid_date('  ') == ''
+
+@pytest.mark.parametrize('test_input, expected', [
+    pytest.param('202', '202', marks=pytest.mark.xfail(reason='Invalid date format')),
+])
+def test_invalid_date(test_input: str, expected: int):
+    assert valid_date(test_input) == expected
